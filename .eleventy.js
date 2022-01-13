687e332197a8375d4ffc967814b49f378fc6fac6
tentacle <my.tentacles.are.in.you@gmail.com>
2022-01-13 19:36:20 +0200

    lang

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig){
 
     eleventyConfig.addPassthroughCopy('./src/css')
     eleventyConfig.addPassthroughCopy('./src/robots.txt')
+    eleventyConfig.addPassthroughCopy('./src/ru')
+    eleventyConfig.addPassthroughCopy('./src/en')
     eleventyConfig.addPassthroughCopy('./src/assets')
 
     return {
